6ac545833a024bb993b5dec7c7f0ca49a049982d
rujinlong <jinlongru@gmail.com>
2023-09-01 17:04:02 +0200

    fix sample

--- bin/create_samplesheet.sh
+++ bin/create_samplesheet.sh
@@ -7,4 +7,9 @@ for f in $(ls fastq/*_R1.fq.gz);do
     echo "${fid},$(pwd)/fastq/${fid}_R1.fq.gz,$(pwd)/fastq/${fid}_R2.fq.gz" >> sample.csv
 done
 
+for f in $(ls fastq/*_R1.fq.gz | grep -v "_R1.fq.gz" | grep -v "_R2.fq.gz");do
+    fid=$(echo $f | sed 's/.fq.gz//g' | cut -d'/' -f2)
+    echo "${fid},$(pwd)/fastq/${fid}.fq.gz," >> sample.csv
+done
+
 head -n3 sample.csv > test_sample.csv
